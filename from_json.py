639121a973e6749ce8d0f8433c5130a3f17eebdf
tckz <at.tckz@gmail.com>
2025-10-22 19:52:32 +0900

    add: from_json

---
+++ from_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+import pandas as pd
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("--input", required=False)
+args = arg_parser.parse_args()
+
+fn = sys.stdin if args.input is None else args.input
+
+df = pd.read_json(fn, lines=True)
+
+df.to_csv(sys.stdout, sep="\t", index=False)
