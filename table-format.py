b514df35d4e8720f5eed5b81f4c951ce9b620f16
tckz <at.tckz@gmail.com>
2025-10-22 19:54:59 +0900

    table-format: --format

--- table-format.py
+++ table-format.py
@@ -4,10 +4,11 @@ import argparse
 import sys
 
 import pandas as pd
-from tabulate import tabulate
+from tabulate import tabulate, tabulate_formats
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument("--input", required=False)
+arg_parser.add_argument("--format", required=False, default="simple", choices=tabulate_formats)
 args = arg_parser.parse_args()
 
 fn = sys.stdin if args.input is None else args.input
