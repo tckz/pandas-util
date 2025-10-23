0f69e512cc7abf85cf4e5ec12f5dd53bfb6eea46
tckz <at.tckz@gmail.com>
2025-10-23 09:44:09 +0900

    query: more help

--- query.py
+++ query.py
@@ -8,9 +8,9 @@ import pandas as pd
 # --query 'ignore_webhook!=True'
 
 arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument("--query", required=True)
+arg_parser.add_argument("--query", required=True, help="e.g. 'ignore_webhook!=True', 'col2>10', 'col1==\"abc\"'")
 arg_parser.add_argument("--input", required=False)
-arg_parser.add_argument("--engine", required=False)
+arg_parser.add_argument("--engine", required=False, help="e.g. python, numexpr")
 args = arg_parser.parse_args()
 
 fn = sys.stdin if args.input is None else args.input
